b0c7d50e3da0f05ec4de7a9d9cfefb0af947abab
Morojgovany <24272862+morojgovany@users.noreply.github.com>
2025-04-08 18:43:38 +0200

    Add cancel anim key check

--- config.lua
+++ config.lua
@@ -1,3 +1,4 @@
 Config = {}
 Config.command = 'bazooka'
-Config.adminCheck = false
+Config.adminCheck = false -- if true, admin must be set in perms.cfg
+Config.disableCancelAnimationKey = 0x4AF4D473 -- your key to cancel animations
